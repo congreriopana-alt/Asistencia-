32abc6e91ecbaaed9bb6a31be437465617585ce7
congreriopana-alt <congreriopana@gmail.com>
2026-08-19 17:11:44 -0600

    Add files via upload

---
+++ sw-dash.js
@@ -0,0 +1,43 @@
+const CACHE = 'elrio-dash-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        '/Asistencia-/dashboard_el_rio.html',
+        '/Asistencia-/manifest-dash.json',
+        '/Asistencia-/icon-dash.png',
+        '/Asistencia-/icon-dash-192.png'
+      ]).catch(() => {})
+    )
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = e.request.url;
+  if (url.includes('google.com') || url.includes('gstatic.com')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('',{status:503})));
+    return;
+  }
+  e.respondWith(
+    caches.open(CACHE).then(cache =>
+      cache.match(e.request).then(cached => {
+        const net = fetch(e.request).then(res => {
+          if (res && res.status === 200) cache.put(e.request, res.clone());
+          return res;
+        }).catch(() => cached);
+        return cached || net;
+      })
+    )
+  );
+});
